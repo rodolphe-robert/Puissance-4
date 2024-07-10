3f74977b70b2cf768587f821913f2345a376c395
rodolphe.robert <rodolphe.robert@le-campus-numerique.fr>
2024-07-10 11:48:40 +0200

    Board

---
+++ puissance.py
@@ -0,0 +1,30 @@
+print("+---+---+---+---+---+---+---+---+")
+print("\n")
+
+count_tableau = 0
+ligne = 8
+while count_tableau < ligne:
+    
+    count = 0
+    colonne=6
+    while  count <= colonne -1:
+        print("+---",end="")
+        count += 1
+    print("+")
+
+    count_tilt = 0
+    while count_tilt <= colonne-1:
+        print("|   ", end="")
+        count_tilt += 1
+    print("|")
+
+    count_tableau += 1
+
+count_fin =0
+while  count_fin <= colonne -1:
+        print("+---",end="")
+        count_fin += 1 
+print("+")
+
+
+print("\n")
